d75eafa8a13af5064b099afea8843d1686d5c1de
DominiqueFlorence <ddaniell1987@gmail.com>
2021-12-07 21:06:14 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function writeCards(names, eventName) {
-    const messagesArray = [];
-    for (let i = 0; i < names.length; i++) {
-        messagesArray[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    const cards = [];
+    for (let i = 0; i < names.length; i++){
+        cards[i] = `I love you ${names}, thanks for the wonderful ${eventName}!`;
     }
-    return messagesArray;
+    return cards;
 }
 
-function countDown(number) {
-    while (number>=0) {
-        console.log(number);
-        number--;
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
     }
 }
+return countDown(10);
